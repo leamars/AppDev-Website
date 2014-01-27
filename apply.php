109aa166db06ef1f7450f15b82675237728972b8
Lea Marolt Sonnenschein <maroltso@grinnell.edu>
2014-01-26 22:38:28 -0600

    Finished website version 1.0

--- apply.php
+++ apply.php
@@ -1,100 +1,173 @@
 <?php 
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+	
+	$name = trim($_POST["name"]);
+    $email = trim($_POST["email"]);
+    $position = trim($_POST["position"]);
+    $project = trim($_POST["project"]);
+    $q1 = trim($_POST["q1"]);
+    $q2 = trim($_POST["q2"]);	
+    $q3 = trim($_POST["q3"]);
+    
+    if ($name == "" or $email == "" or $position == "" or $q1 == "" or $q2 == "" or $q3 == "") {
+        header("Location: apply.php?status=incomplete");
+        exit;
+    }
+    
+    foreach($_POST as $value) {
+        if(stripos($value, 'Content-Type:') !== FALSE) {
+            header("Location: apply.php?status=spam");
+            exit;
+        }
+    }
+    
+    if($_POST["address"] != "") {
+        header("Location: apply.php?status=spam");
+        exit;
+    }
+    
+    require_once("inc/phpMAILER/class.phpmailer.php");
+    $mail = new PHPMailer();
+    
+    if (!$mail->ValidateAddress($email)) {
+        header("Location: apply.php?status=invalidEmail");
+        exit;
+    }
+
+	$email_body = "";
+
+    $email_body = $email_body 
+    . "Name: " . $name . "<br />" 
+    . "Email: " . $email . "<br />" 
+    . "Position: " . $position . "<br />"
+    . "Why do you want to join? " . $q1 . "<br />"
+    . "What will you bring to the table? " . $q2 . "<br />"
+    . "Random questions? " . $q3;
+
+    $mail->SetFrom($email, $name);
+    $address = "appdev@grinnell.edu";
+    $mail->AddAddress($address, "Grinnell AppDev");
+    $mail->Subject    = "Grinnell AppDev Application Form | " . $name;
+    $mail->MsgHTML($email_body);
+
+    if(!$mail->Send()) {
+        echo "Mailer Error: " . $mail->ErrorInfo;
+        mail($address, $subject, $email_body, $headers);
+        exit;
+    } 
+
+    header("Location: apply.php?status=thanks");
+    exit;
+}
+?>
+
+<?php 
 $activeClass = "apply";
 include('inc/header.php'); 
 ?>
+
+    <link rel="shortcut icon" href="favicon.png">
 
     <div class="apply">
         <div class="color">
             <div class="apply">
                 <div class="container">
                     <div class="page-header main-content">
-                        <h1>You wanna make them, too?!</h1>
+                        <h1 class="redTitle">You wanna make apps, too?!</h1>
 
-                        <h3>Go right ahead, person!</h3>
+                        <h3 class="redTitle">We're currently looking for:</h3>
 
-                        <div class="col-lg-3"></div>
-
-                        <div class="col-lg-6">
-                            <div class="bottom">
-                                <p class="bottom">If you want to be a part of our totally <em>awesome</em> team, please fill out this preliminary questionaire, and write-up prompt, so we can better gauge where we can fit you in (:</p>
-                            </div>
-
-                            <div class="col-lg-2"></div>
-                        </div>
+                    <div id="jobOfferings">
+					<div class="col-lg-3">
+					<div class="jobPic" id="devs">
+						<img class="responsive jobs" src="img/developper.png" />
+						<h2 id="developers"><span>Engineering</span></h2>
+						</div>
+					</div>
+					</div>
+					<div class="col-lg-3">
+					<div class="jobPic" id="desi">
+						<img class="responsive jobs" src="img/designer.png">
+						<h2 id="designers"><span>(UX) Design</span></h2>
+					</div>
+					</div>
+					<div class="col-lg-3">
+					<div class="jobPic" id="uxd">
+						<img class="responsive jobs" src="img/ux.png">
+						<h2 id="ux"><span>Finance</span></h2>
+						
+					</div>
+					</div>
+					<div class="col-lg-3">
+					<div class="jobPic" id="comm">
+						<img class="responsive jobs" src="img/community.png" />
+						<h2 id="community"><span>Community</span></h2>
+						</div>
+					</div>
+					</div>
+					</div>	
+					
+					<div class="col-lg-12"></div>
+			
+										
                     </div>
                 </div>
             </div>
         </div>
-
+        
         <div class="container">
-            <div class="applyCont">
-                <div class="col-lg-3"></div>
+        
+        <div class="col-lg-3"></div>
+        
+        <div class="col-lg-6 jobRole">
+        	        
+        </div>
+        
+         <div class="col-lg-3"></div>
+        
+         <div class="col-lg-12"></div>
+        
+        <div class="col-lg-3"></div>
+        <div class="col-lg-6">
 
-                <div class="col-lg-6">
-                    <h2>Personal Information</h2>
+	        <h2 class="center">Apply!</h2>
+	        
+	        <?php if (isset($_GET["status"]) and $_GET["status"] == "thanks") { ?>
+            <p class="redForm">Thanks for applying, we'll be in touch shortly!</p>
+            <?php } if (isset($_GET["status"]) and $_GET["status"] == "spam") { ?>
+            <p class="redForm">Are you trying to spam us? Shame on you!</p>
+            <?php } if (isset($_GET["status"]) and $_GET["status"] == "invalidEmail") { ?>
+            <p class="redForm">The email you provided seems to be invalid. Please try again.</p>
+            <?php } if (isset($_GET["status"]) and $_GET["status"] == "incomplete") { ?>
+            <p class="redForm">Hey, you! You sent us an incomplete form. Shame on you! Please try again.</p> <?php } ?> 
+	        
+            <form class="form" action="apply.php" method="post" id="apply">
+            <fieldset>
+             
+            <label for="name">Name: *</label> <br />
+            <input type="text" id="name" name="name"><br />
+            <label for="email">Email: *</label> <br />
+            <input type="text" id="email" name="email"><br />
+            <label for="position">Position: *</label> <br />
+            <input type="text" id="position" name="position"><br />
+            <label for="project">Projects/Portfolio: </label> <br />
+            <input type="text" id="project" name="project"><br />
+            <label for="q1">Why do you want to join?</label> (300 words or less)<br />
+            <textarea name="q1" rows="5" cols="80" maxlength="1000" id="q1"></textarea> <br />
+            <label for="q2">What will you bring to the table?</label> (300 words or less)<br />
+            <textarea name="q2" rows="5" cols="80" maxlength="1000" id="q2"></textarea> <br />
+            <label for="q3">Random Question:</label> (300 words or less)<br />
+            <textarea name="q3" rows="5" cols="80" maxlength="1000" id="q3"></textarea> <br /> <br />
+            <button onClick = <?php $success = true; ?>type="submit">Submit</button>
 
-                    <div class="control-group"></div>
+            </fieldset>
+            </form>
+        </div>
+        <div class="col-lg-3"></div>  
 
-                    <div class="control-group">
-                        <label for="jvfirstname">First Name: <span class="required">*</span></label>
-
-                        <div class="controls">
-                            <input name="jvfirstname" id="jvfirstname" type="text" value="" class="jvtext" autofocus="">
-                        </div>
-                    </div>
-
-                    <div class="control-group">
-                        <label for="jvlastname">Last Name: <span class="required">*</span></label>
-
-                        <div class="controls">
-                            <input name="jvlastname" id="jvlastname" type="text" value="" class="jvtext">
-                        </div>
-                    </div>
-
-                    <div class="control-group">
-                        <label for="jvemail">Email: <span class="required">*</span></label>
-
-                        <div class="controls">
-                            <input name="jvemail" id="jvemail" type="text" value="" class="jvtext">
-                        </div>
-                    </div>
-
-                    <div class="control-group">
-                        <label for="jvphone">Phone:</label>
-
-                        <div class="controls">
-                            <input name="jvphone" id="jvphone" type="text" size="20" maxlength="20" class="jvtext">
-                        </div>
-                    </div>
-
-                    <div class="control-group jvcoverletter">
-                        <label for="jvcoverletter">What inspires you:</label>
-
-                        <div class="controls">
-                            <div class="coverletter">
-                                <a id="ImportCL" href="#" onclick="jvAddAttachment2('jvcoverletter', 'qG29Vfww'); return false;" class="lu">Insert a document</a>&nbsp;(optional)
-
-                                <div style="display:none;" name="RemoveCL" id="RemoveCL">
-                                    <b>Cover Letter Uploaded <a href="#" onclick="removeDoc('jvcoverletter'); return false;">[remove]</a></b>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-
-                    <div class="control-group">
-                        <div class="controls">
-                            <textarea name="jvcoverletter" id="jvcoverletter" class="textarea jvtextwidth" rows="10" cols="100">
-</textarea>
-                        </div>
-                    </div>
-                </div>
-
-                <div class="col-lg-3"></div>
-            </div>
         </div>
         </div>
     </div><?php 
     include('inc/footer.php'); 
     ?>
-</body>
-</html>
--- index.php
+++ index.php
@@ -13,9 +13,11 @@ include('inc/header.php');
     <div class="jumbotron">
         <div class="container">
             <div class="page-header main-content">
-                <h1>'sup!</h1>
+                <h1><img class="logo" src="img/adLogo.png" /></h1>
 
                 <h3>We make mobile apps!</h3>
+                
+             
             </div>
 
             <div class="col-lg-1"></div>
@@ -27,59 +29,6 @@ include('inc/header.php');
             <div class="col-lg-1"></div>
         </div>
     </div>
-
-    <div class="container">
-        <div class="col-lg-6">
-            <h1 id="ad">Grinnell AppDev</h1>
-
-            <p class="lead">... is a student-based team from <em>Grinnell College</em> that designs, develops, and delopys mobile applications on the <em>iOS</em> and <em>Android</em> platform. We come from different backgrounds, and each one of us brings something unique and valueable to the team.</p>
-        </div>
-
-        <div class="col-lg-1"></div>
-
-        <div class="col-lg-5">
-            <p></p><img class="img-responsive" src="img/iowa.png">
-
-            <p class="lead">You can find our headquarters in <em>Grinnell, IA</em>.</p>
-        </div>
-    </div>
-
-    <div class="color">
-        <div class="container">
-        
-	        <div class="col-lg-3"></div>
-            <div class="col-lg-6">
-            <div class="bottom">
-	            <p class="bottom">We create quality applications that are beautiful, self-evident, intuitive, and well implemented!</p>
-            </div>
-            </div>
-            <div class="col-lg-3"></div>
-        </div>
-        
-        <div class="container">
-        	
-            <div class="page-header main-content">
-                <h1>Because we are...</h1>
-            </div>
-
-            
-            <div class="col-lg-4"> 
-            <img class="img-responsive center" src="img/development.png" />
-            <p class="lead" id="imgtitle">Crafty developers!</p> </div>
-      
-            
-            <div class="col-lg-4"> 
-            <img class="img-responsive center" src="img/design.png" />
-            <p class="lead" id="imgtitle">Creative designers!</p> </div>
-            
-            
-            <div class="col-lg-4"> 
-            <img class="img-responsive center" src="img/enterpreneur.png" />
-            <p class="lead" id="imgtitle">Cute enterpreneurs!</p> </div>
-
-        </div>
-
-        </div>
         
     </div>
 
